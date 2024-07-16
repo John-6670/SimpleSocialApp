d551f4753780a2c4a8662418a229c1246e1b56e5
John_6670 <mayn2005m@gmail.com>
2024-07-16 23:54:12 +0330

    with bug

--- posts/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.PostListCreate.as_view(), name='list-notes'),
-    path('<int:id>/', views.PostRetrieveUpdateDestroy.as_view(), name='RUD-note'),
-]
