3a62f06e732f6c3f48a13bc25cec2309082b386e
John <john2005mm@gmail.com>
2024-07-19 15:22:33 +0330

    updated profiles

--- users/serializers.py
+++ users/serializers.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers, viewsets
 from rest_framework.authtoken.admin import User
 
+from posts.models import Post
+from posts.serializers import PostListCreateSerializer
+
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
     confirm_password = serializers.CharField(write_only=True)
@@ -22,10 +25,17 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
 
 
 class UserInformationSerializer(serializers.ModelSerializer):
+    posts = serializers.SerializerMethodField()
+
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'first_name', 'last_name']
-        read_only_fields = ['username', 'id']
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'posts']
+        read_only_fields = ['username', 'id', 'posts']
+
+    def get_posts(self, obj):
+        posts = Post.objects.filter(author=obj)
+        request = self.context.get('request')
+        return PostListCreateSerializer(posts, many=True, context={'request': request[]}).data
 
 
 class UserLoginSerializer(serializers.Serializer):
