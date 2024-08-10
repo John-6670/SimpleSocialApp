3d0dc903908c392522f872c0057e7175f87f0aec
John_6670 <mayn2005m@gmail.com>
2024-08-10 18:10:09 +0330

    update

---
+++ utils/utils.py
@@ -0,0 +1,20 @@
+import jwt
+import os
+from django.core.mail import EmailMessage
+from dotenv import load_dotenv
+from datetime import datetime, timedelta, UTC
+
+load_dotenv()
+
+
+class EmailHelper:
+    @staticmethod
+    def send_email(user):
+        token = jwt.encode({'user_id': user.id, 'exp': datetime.now(UTC) + timedelta(hours=1)},
+                           os.environ.get('SECRET_KEY'), algorithm='HS256')
+        email_subject = 'Verify your email'
+        email_body = (f'Hi {user.username},\n'
+                      f'Use the link below to verify your email:\n'
+                      f'http://localhost:8000/users/verify-email/?token={token}')
+        email = EmailMessage(subject=email_subject, body=email_body, to=[user.email])
+        email.send()
