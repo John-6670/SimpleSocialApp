d5486b89ea0fa9512df36d0f909929ca8e2cd981
John_6670 <mayn2005m@gmail.com>
2024-08-15 16:33:36 +0330

    creating models

--- users/models.py
+++ users/models.py
@@ -1,24 +1,52 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
 
 # Create your models here.
+class CustomUserManager(BaseUserManager):
+    def create_user(self, **user_data):
+        email = user_data.get('email')
+        username = user_data.get('username')
+
+        if not email:
+            raise ValueError('The Email field must be set')
+        if not username:
+            raise ValueError('The Username field must be set')
+
+        email = self.normalize_email(email)
+        user = self.model(**user_data)
+        user.set_password(user_data.get('password'))
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, **user_data):
+        user = self.create_user(**user_data)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=150, unique=True)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    bio = models.TextField(blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    profile_pic = models.ImageField(upload_to='avatars/', blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+
 class Follow(models.Model):
-    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following')
-    following = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followers')
+    follower = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='following')
+    following = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='followers')
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         unique_together = ('follower', 'following')
-
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField(blank=True)
-    birth_date = models.DateField(null=True, blank=True)
-    profile_pic = models.ImageField(upload_to='avatars/', blank=True, null=True)
-
-    def __str__(self):
-        return self.user.username
